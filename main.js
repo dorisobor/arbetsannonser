5b1bb2fe63efbd8105bc295a646a272354a02fb7
dorisobor <troyamber@live.com>
2018-04-10 20:21:39 +0200

    add view all jobs function

---
+++ main.js
@@ -0,0 +1,56 @@
+const latestJobs = document.getElementById("latest-jobs");
+
+fetchStockholmJobs();
+
+
+function fetchStockholmJobs(){
+
+fetch("http://api.arbetsformedlingen.se/af/v0/platsannonser/matchning?lanid=1&sida=1&antalrader=20")
+  .then((response) => response.json())
+  .then((jobs) => {
+    displayJob(jobs)
+   })
+
+.catch((error) => {
+  console.log(error)
+});
+}
+
+
+function displayJob(jobs) {
+  const job = jobs.matchningslista.matchningdata;
+  const latestJobs = document.getElementById("latest-jobs");
+let text = "";
+
+
+
+for (let i = 0; i <  job.length; i++) {
+//  job.sort(function(a, b){
+//     var x = a.publiceraddatum;
+//     var y = b.publiceraddatum;
+//     if (x < y) {return 1;}
+//     if (x > y) {return -1;}
+//     return 0;
+// });
+ 
+text += `<table>
+<tr>
+<th>Titel</th>
+<th><p>Kommun</th>
+<th><p>Publicerad</th>    
+</tr>
+<tr>
+<td>${job[i].annonsrubrik}</td>
+<td>${job[i].kommunnamn}</td>
+<td>${job[i].sista_ansokningsdag} </td> 
+</tr>
+</table> `;
+}
+
+document.getElementById("all-jobs").innerHTML = text;
+
+
+      
+
+
+}
